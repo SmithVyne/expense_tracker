3ab715b2c3e0961b67da9e568220924d82aa07f6
SmithVyne <smithnkereuwem2@gmail.com>
2021-02-15 07:34:04 +0300

    update expenses controller

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,7 +3,7 @@ class ExpensesController < ApplicationController
 
   # GET /expenses
   def index
-    @expenses = Expense.all.group_by{|c| c.category_id
+    @expenses = Expense.all.group_by{|c| c.category_id}
 
     render json: @expenses
   end
